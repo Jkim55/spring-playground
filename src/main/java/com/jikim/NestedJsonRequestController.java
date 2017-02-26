20303848680bd824e4c0cdf78c43c400ddc0973f
Ji Kim <jkim55@gmail.com>
2017-02-25 19:42:56 -0700

    finished test for Nests Json from String Literal test and class

--- src/main/java/com/jikim/NestedJsonRequestController.java
+++ src/main/java/com/jikim/NestedJsonRequestController.java
@@ -1,7 +1,96 @@
 package com.jikim;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.sun.tools.javac.util.ArrayUtils;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@RestController
+@RequestMapping("/flight")
 public class NestedJsonRequestController {
 
+    @PostMapping("/string")
+    public String getDataAsStringLiteral(@RequestBody Flight flight){
+        return String.format(
+                "Flight Details: Destination: %s | Departs: %s | Pilot: %s | Passengers: %s",
+                flight.getDestination(),
+                flight.getDepartsAt(),
+                flight.getPilot().getName(),
+                flight.getPassengers()
+        );
+    }
 
+    static class Flight {
+        @JsonIgnore
+        private int id;
+        private String destination;
+
+        @JsonProperty("departs")
+        private Date departsAt;
+        private Person pilot;
+        private List<Person> passengers;
+
+        public int getId() {
+            return id;
+        }
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public String getDestination() {
+            return destination;
+        }
+        public void setDestination(String destination) {
+            this.destination = destination;
+        }
+
+        public Date getDepartsAt() {
+            return departsAt;
+        }
+        public void setDepartsAt(Date departsAt) {
+            this.departsAt = departsAt;
+        }
+
+        public Person getPilot() {
+            return pilot;
+        }
+        public void setPilot(Person pilot) {
+            this.pilot = pilot;
+        }
+
+        public String getPassengers() {
+            ArrayList<String> passengerList = new ArrayList<String>();
+
+            for (Person passenger : passengers){
+                passengerList.add(passenger.name);
+            }
+            return String.join(", ", passengerList);
+        }
+
+        public void setPassengers(List<Person> passengers) {
+            this.passengers = passengers;
+        }
+
+        static class Person {
+            private final String name;
+
+            @JsonCreator
+            public Person(@JsonProperty("name") String name) {
+                this.name = name;
+            }
+
+            public String getName() {
+                return name;
+            }
+        }
+
+    }
 }
--- src/test/java/com/jikim/NestedJsonRequestControllerTest.java
+++ src/test/java/com/jikim/NestedJsonRequestControllerTest.java
@@ -4,11 +4,17 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest(JsonRequestController.class)
+@WebMvcTest(NestedJsonRequestController.class)
 
 public class NestedJsonRequestControllerTest{
 
@@ -17,21 +23,27 @@ public class NestedJsonRequestControllerTest{
 
     @Test
     public void fetchAsStringLiteral() throws Exception {
+        MockHttpServletRequestBuilder request = post("/flight/string")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\"id\": 10,\"destination\": \"London\",\"departsOn\": \"2017-04-06\",\"passengers\": [{\"name\": \"Eliza\"}, {\"name\": \"Samuel\"}],\"pilot\": {\"name\": \"Angelica\"}}");
 
+        this.mvc.perform(request)
+                .andExpect(status().isOk())
+                .andExpect(content().string("Flight Details: Destination: London | Departs: null | Pilot: Angelica | Passengers: Eliza, Samuel"));
     }
 
-    @Test
-    public void fetchWithGSONBuilder() throws Exception {
-
-    }
-
-    @Test
-    public void fetchWithGSONSerializedObject() throws Exception {
-
-    }
-
-    @Test
-    public void fetchAsRawBodyFromFileFixturePull() throws Exception {
-
-    }
+//    @Test
+//    public void fetchWithGSONBuilder() throws Exception {
+//
+//    }
+//
+//    @Test
+//    public void fetchWithGSONSerializedObject() throws Exception {
+//
+//    }
+//
+//    @Test
+//    public void fetchAsRawBodyFromFileFixturePull() throws Exception {
+//
+//    }
 }
